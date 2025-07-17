d16dfd816a0f75f47d257d3c7497a7d7d795a754
anandstomar <anandsinghtomar25.com>
2025-07-17 16:44:08 +0530

    candidate

--- dist/models/detailsModel.js
+++ dist/models/detailsModel.js
@@ -2,16 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
 const EducationSchema = new mongoose_1.Schema({
-    degree: { type: String, required: true },
     college: { type: String, required: true },
+    collegeDegree: { type: String, required: true },
     branch: { type: String },
     passingYear: { type: Number, required: true, min: 1900, max: new Date().getFullYear() },
     cgpa: { type: Number, required: true, min: 0, max: 10 },
+    board10: { type: String, required: true },
+    percentage10: { type: Number, required: true, min: 0, max: 100 },
+    board12: { type: String, required: true },
+    percentage12: { type: Number, required: true, min: 0, max: 100 },
 }, { _id: false });
 const CandidateSchema = new mongoose_1.Schema({
-    userId: { type: mongoose_1.Schema.Types.ObjectId, ref: 'User', required: true },
-    fullName: { type: String, required: true, ref: 'User' },
-    email: { type: String, required: true, ref: 'User' },
+    userId: { type: mongoose_1.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
+    fullName: { type: String, required: true },
+    email: { type: String, required: true },
     dob: { type: Date, required: true },
     phone: { type: String, required: true, unique: true },
     education: { type: [EducationSchema], required: true },
