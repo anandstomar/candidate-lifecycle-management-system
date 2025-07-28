b696d86d5caf5d3a18f1d1ddc965535cc201abc3
anandstomar <anandsinghtomar25.com>
2025-07-28 11:15:02 +0530

    shxjvsh

--- dist/routes/authRoutes.js
+++ dist/routes/authRoutes.js
@@ -4,8 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const authMiddleware_1 = require("../middleware/authMiddleware");
 const authController_1 = require("../controllers/authController");
 const router = express_1.default.Router();
 router.post('/register', authController_1.register);
 router.post('/login', authController_1.login);
+router.post('/logout', authMiddleware_1.authenticateUser, authController_1.logout);
+router.post('/make-payment', authMiddleware_1.authenticateUser, authController_1.makePayment);
+router.post('/verify-payment', authMiddleware_1.authenticateUser, authController_1.verifyPayment);
 exports.default = router;
